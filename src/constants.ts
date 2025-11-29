e524101b7224ebbdbb54ba3b1989804201562b19
Sohaieb Azaiez <sohaiebazaiezprof@gmail.com>
2025-11-29 23:11:16 +0100

    release/0.0.5

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export default {
   PROJECT_NAME: "VNscanner",
-  PROJECT_VERSION: "0.0.4",
+  PROJECT_VERSION: "0.0.5",
   getZipConfig: function () {
     return {
       outputFilePath: `./output/${this.PROJECT_NAME.toLocaleLowerCase()}-${this.PROJECT_VERSION.toLocaleLowerCase()}.zip`,
